427c227bd13f2cdb6dc70c2911b98bf8635cd939
okoli-ryan <okoliryan50@gmail.com>
2023-10-15 16:13:04 +0100

    setup push notifications

---
+++ Controllers/PushNotificationController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pusher.PushNotifications;
+
+namespace OrderUp_API.Controllers {
+    [Route("pusher")]
+    [ApiController]
+    public class PushNotificationController : ControllerBase {
+
+        readonly PushNotifications pushNotifications;
+
+        public PushNotificationController() {
+            var pushNotificationsOptions = new PushNotificationsOptions {
+                InstanceId = "YOUR_INSTANCE_ID_HERE",
+                SecretKey = "YOUR_SECRET_KEY_HERE"
+            };
+
+            pushNotifications = new PushNotifications(new HttpClient(), pushNotificationsOptions);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GenerateToken() {
+
+            var token = pushNotifications.GenerateToken();
+
+        }
+    }
+}
---
+++ Services/PushNotificationService.cs
@@ -0,0 +1,36 @@
+﻿using PusherServer;
+
+namespace OrderUp_API.Services {
+    public class PushNotificationService {
+
+        readonly PushNotifications pushNotifications;
+        readonly HttpContext context;
+
+        public PushNotificationService(IHttpContextAccessor contextAccessor) {
+            var pushNotificationsOptions = new PushNotificationsOptions {
+                InstanceId = ConfigurationUtil.GetConfigurationValue("Pusher-Beams:INSTANCE_ID"),
+                SecretKey = ConfigurationUtil.GetConfigurationValue("Pusher-Beams:SECRET_KEY")
+            };
+
+            pushNotifications = new PushNotifications(new HttpClient(), pushNotificationsOptions);
+            context = contextAccessor.HttpContext;
+        }
+
+
+        public DefaultResponse<string> GenerateToken() {
+
+            var UserID = GetJwtValue.GetTokenFromCookie(context, ClaimTypes.PrimarySid);
+
+            if (string.IsNullOrEmpty(UserID)) return new DefaultErrorResponse<string> {
+                ResponseCode = ResponseCodes.UNAUTHORIZED,
+                ResponseMessage = ResponseMessages.UNAUTHORIZED,
+                ResponseData = null
+            };
+
+            var token = pushNotifications.GenerateToken(UserID);
+
+            return new DefaultSuccessResponse<string>(token);
+
+        }
+    }
+}
